d0c7abe815b879d64de969b83380da521805243d
王宪鹏 <wang.xianpeng@xcar.com.cn>
2019-04-16 18:31:17 +0800

    test

---
+++ project-parcel/index.js
@@ -0,0 +1,5 @@
+import './index.css';
+import test from './test.js';
+
+console.log(test);
+console.log('hello world');
---
+++ project-parcel/test.js
@@ -0,0 +1,6 @@
+let test = {
+    a: 1,
+    b: 2
+}
+
+module.exports = test;
