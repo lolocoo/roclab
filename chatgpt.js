ace8bbe84d842ef959ded6365d9626061c73dd40
lolocoo <lolocoo@gmail.com>
2023-02-01 21:26:18 +0800

    Create chatgpt.js

---
+++ chatgpt.js
@@ -0,0 +1,14 @@
+
+curl https://api.openai.com/v1/models \
+  -H 'Authorization: Bearer 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' \
+  -H 'OpenAI-Organization: org-46egR71xnHn3VMvtHAL0qe4C'
+
+
+curl https://api.openai.com/v1/images/generations \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer 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' \
+  -d '{
+  "prompt": "A real dancing girls ride a white horse",
+  "n": 2,
+  "size": "1024x1024"
+}'
