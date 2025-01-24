c67883f594cee2038931980c6bc12678532cd0b4
lolocoo <lolocoo@gmail.com>
2025-01-24 17:03:43 +0800

    Update entrypoint.sh

--- files/trocr-chinese/entrypoint.sh
+++ files/trocr-chinese/entrypoint.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
-source ~/.bashrc
+#!/bin/bash --login
+# The --login ensures the bash configuration is loaded,
+# enabling Conda.
+
+# Enable strict mode.
+set -euo pipefail
+# ... Run whatever commands ...
+
+# Temporarily disable strict mode and activate conda:
+set +euo pipefail
 conda activate seal
-exec "$@"
+
+# Re-enable strict mode:
+set -euo pipefail
